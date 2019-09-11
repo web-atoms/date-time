c42389e5cd1d8f9f60b1954b7ba2db9c1410ed1b
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-09-11 12:38:36 +0530

    wip

--- src/TimeSpan.ts
+++ src/TimeSpan.ts
@@ -10,7 +10,15 @@ export const msHours = 3600000;
 
 export const msDays = 24 * msHours;
 
-const timeSpanFormat = /(?<hours>\d{1,2}):(?<mins>\d{1,2})(?<ampm>\s+(am|pm)?)/gi;
+const timeSpanFormat = /(?<hours>\d{1,2}):(?<mins>\d{1,2})(:(?<seconds>\d{1,2}))?(?<ampm>\s+(am|pm)?)/gi;
+
+function padLeft(n: number, c: number, t: string = "0"): string {
+    let s = n.toString();
+    if (s.length < c) {
+       s = t + s;
+    }
+    return s;
+}
 
 export default class TimeSpan {
 
@@ -32,15 +40,56 @@ export default class TimeSpan {
 
     public static parse(text: string): TimeSpan {
         const a = timeSpanFormat.exec(text) as any;
-        if (a.groups) {
-            
+        const ag = a ? a.groups : null;
+        if (!ag) {
+            throw new Error("Invalid time format");
         }
+        let h = parseInt(ag.hours || "0", 10);
+        const m = parseInt(ag.mins || "0", 10);
+        const s = parseInt(ag.seconds || "0", 10);
+        const ampm = ag.ampm;
+        if (ampm && /pm/i.test(ampm)) {
+            h += 12;
+        }
+        return new TimeSpan(0, h, m, s);
     }
 
     public milliseconds: number;
 
+    public get totalSeconds(): number {
+        return this.milliseconds / msSeconds;
+    }
+
+    public get totalMinutes(): number {
+        return this.milliseconds / msMinutes;
+    }
+
+    public get totalHours(): number {
+        return this.milliseconds / msHours;
+    }
+
+    public get totalDays(): number {
+        return this.milliseconds / msDays;
+    }
+
+    /**
+     * Duration is always positive TimeSpan
+     */
+    public get duration(): TimeSpan {
+        const t = this.milliseconds;
+        return new TimeSpan(t > 0 ? t : -t);
+    }
+
+    /**
+     * Removes days and only trims given TimeSpan to TimeOfDay
+     */
+    public get trimmedTime(): TimeSpan {
+        return new TimeSpan(Math.ceil(this.milliseconds % msDays));
+    }
+
     constructor(ms: number);
-    constructor(days: number, hours: number, minutes: number, seconds: number, milliseconds: number)
+    // tslint:disable-next-line: unified-signatures
+    constructor(days: number, hours: number, minutes?: number, seconds?: number, milliseconds?: number)
     constructor(days: number, hours?: number, minutes?: number, seconds?: number, milliseconds?: number) {
         if (arguments.length === 1) {
                 this.milliseconds = days;
@@ -54,27 +103,31 @@ export default class TimeSpan {
         }
     }
 
-    public totalSeconds(): number {
-        return this.milliseconds / msSeconds;
-    }
-
-    public totalMinutes(): number {
-        return this.milliseconds / msMinutes;
-    }
-
-    public totalHours(): number {
-        return this.milliseconds / msHours;
-    }
-
-    public totalDays(): number {
-        return this.milliseconds / msDays;
-    }
-
     /**
-     * Duration is always positive TimeSpan
+     * Format the TimeSpan as time format
+     * @param formatAs24 Display time as 24 hours
      */
-    public duration(): TimeSpan {
-        const t = this.milliseconds;
-        return new TimeSpan(t > 0 ? t : -t);
+    public toString(formatAs24: boolean = false): string {
+        if (this.milliseconds > msDays) {
+            return this.trimmedTime.toString(formatAs24);
+        }
+
+        let h = Math.floor(this.totalHours);
+        const m = Math.floor(this.totalMinutes);
+        const s = Math.floor(this.totalSeconds);
+
+        let postFix = "";
+        if (!formatAs24) {
+            if (h > 12) {
+                postFix = " PM";
+                h -= 12;
+            } else {
+                postFix = " AM";
+            }
+        }
+        if (s > 0) {
+            return `${padLeft(h, 2)}:${padLeft(m, 2)}:${padLeft(s, 2)}${postFix}`;
+        }
+        return `${padLeft(h, 2)}:${padLeft(m, 2)}${postFix}`;
     }
 }
